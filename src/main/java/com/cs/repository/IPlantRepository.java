57b68e67fdee64a09f047c77090d5b9d0cc44de6
hpedamal <hpedamal@LIN24010063.corp.capgemini.com>
2022-03-27 16:18:54 +0530

    Plant + CRUD ops

---
+++ src/main/java/com/cs/repository/IPlantRepository.java
@@ -0,0 +1,13 @@
+package com.cs.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import com.cs.bean.Plant;
+
+public interface IPlantRepository extends JpaRepository<Plant, Integer> {
+		//Get Seed by name
+		@Query(value="select p from Plant p where p.name=:name")
+		public Plant getByName(@Param("name") String name);
+}
