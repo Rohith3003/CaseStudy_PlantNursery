1babd8765a85b282ee9fda007b40ce04c34ee8dd
sapalasrusti <vsapalasrusti@gmail.com>
2022-03-28 19:22:19 +0530

    EndUser Commit

--- src/main/java/com/cs/bean/EndUser.java
+++ src/main/java/com/cs/bean/EndUser.java
@@ -2,16 +2,21 @@ package com.cs.bean;
 
 import java.util.Date;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.EntityListeners;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
 
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
@@ -29,12 +34,9 @@ public class EndUser{
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 	@Column(unique = true)
-	private String emailId;
-	@Column(unique = true)
 	private String mobileNumber;
 	private String fullName;
 	private boolean isAdmin;
-	
 	
 	@CreatedDate
 	@Setter(AccessLevel.PROTECTED)
@@ -43,6 +45,21 @@ public class EndUser{
 	
     @LastModifiedDate
     @Setter(AccessLevel.PROTECTED)
-    Date modifiedAt;	
+    Date modifiedAt;
+    
+    @JsonIgnore
+    @OneToOne(cascade = CascadeType.ALL)
+	@JoinColumn(name = "login_Id")
+	private Login login;
+    
+    @JsonIgnore
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "login_Id")
+	private Address address;
+    
+    @JsonIgnore
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "login_Id")
+	private Cart cart;
     
 }
