eb23145f2e3d08aeb901578fcac200ff1af8d05c
sapalasrusti <vsapalasrusti@gmail.com>
2022-03-28 19:26:26 +0530

    RegisterOutputDto Commit

--- src/main/java/com/cs/dto/RegisterOutputDto.java
+++ src/main/java/com/cs/dto/RegisterOutputDto.java
@@ -1,9 +1,13 @@
 package com.cs.dto;
 
+import com.cs.bean.Address;
 import lombok.Data;
 
 @Data
 public class RegisterOutputDto {
 	private String name;
 	private String emailId;
+	private String mobileNumber;
+	private Integer cartId;
+	private Address address;
 }
