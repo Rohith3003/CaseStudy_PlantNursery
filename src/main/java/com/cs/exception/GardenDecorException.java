b52500fb6ae77567803a6d5e6dc9801e0cc6e3c0
Mayank Kumar <71247904+mayank-kumar29@users.noreply.github.com>
2022-03-30 03:12:28 +0530

    Update GardenDecorException.java

--- src/main/java/com/cs/exception/GardenDecorException.java
+++ src/main/java/com/cs/exception/GardenDecorException.java
@@ -1,5 +1,9 @@
 package com.cs.exception;
-
+/**
+ *  This is a user defined exception class used to generate customized gardendecor exceptions
+ * @author Mayank Kumar(Employee ID: 46191925)
+ *
+ */
 @SuppressWarnings("serial")
 public class GardenDecorException extends RuntimeException
 {
