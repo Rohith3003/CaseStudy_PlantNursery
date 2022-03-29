57ea96820728b087743b5f7942f1ff38b2e30c8a
Mayank Kumar <71247904+mayank-kumar29@users.noreply.github.com>
2022-03-30 03:26:54 +0530

    Update ICartService.java

--- src/main/java/com/cs/service/ICartService.java
+++ src/main/java/com/cs/service/ICartService.java
@@ -1,7 +1,12 @@
 package com.cs.service;
 
 import com.cs.bean.Cart;
-
+/**
+ * This Interface is used to define the services provided to
+ * CartController so that we can view, add to cart and remove from cart into or from database
+ * @author Mayank Kumar(Employee ID: 46191925)
+ *
+ */
 public interface ICartService 
 {
 	Cart viewCart(int cartId);
