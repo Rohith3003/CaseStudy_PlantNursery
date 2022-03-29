a264efbdc14e22d5b128ca3bd36cbaffe6d44c69
Mayank Kumar <71247904+mayank-kumar29@users.noreply.github.com>
2022-03-30 03:00:38 +0530

    Update CartErrorResponse.java

--- src/main/java/com/cs/bean/CartErrorResponse.java
+++ src/main/java/com/cs/bean/CartErrorResponse.java
@@ -2,7 +2,11 @@ package com.cs.bean;
 
 import java.time.LocalDateTime;
 import lombok.Data;
-
+/**
+ * This class is for the Cart Error Response
+ * @author Mayank Kumar(Employee ID: 46191925)
+ *
+ */
 @Data
 public class CartErrorResponse 
 {
