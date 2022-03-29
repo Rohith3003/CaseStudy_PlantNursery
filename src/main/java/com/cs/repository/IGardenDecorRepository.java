fe1aeebc6cde1ea3a73705d0179a6254d40e5cdd
Mayank Kumar <71247904+mayank-kumar29@users.noreply.github.com>
2022-03-30 03:14:44 +0530

    Update IGardenDecorRepository.java

--- src/main/java/com/cs/repository/IGardenDecorRepository.java
+++ src/main/java/com/cs/repository/IGardenDecorRepository.java
@@ -3,7 +3,12 @@ package com.cs.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import com.cs.bean.GardenDecor;
-
+/**
+ * This interface acts as repository which communicates with database and
+ * executes all the queries coming from GardenDecorService to database
+ * @author  Mayank Kumar(Employee ID: 46191925)
+ *
+ */
 @Repository
 public interface IGardenDecorRepository extends JpaRepository<GardenDecor, Integer> {
 
