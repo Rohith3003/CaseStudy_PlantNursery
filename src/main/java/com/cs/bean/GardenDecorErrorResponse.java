0ad8426a19a7b2a9027989c46b629c1892d071a7
Mayank Kumar <71247904+mayank-kumar29@users.noreply.github.com>
2022-03-30 03:01:49 +0530

    Update GardenDecorErrorResponse.java

--- src/main/java/com/cs/bean/GardenDecorErrorResponse.java
+++ src/main/java/com/cs/bean/GardenDecorErrorResponse.java
@@ -2,7 +2,11 @@ package com.cs.bean;
 
 import java.time.LocalDateTime;
 import lombok.Data;
-
+/**
+ * This class is for the Garden Decor Error Response
+ * @author Mayank Kumar(Employee ID: 46191925)
+ *
+ */
 @Data
 public class GardenDecorErrorResponse 
 {
