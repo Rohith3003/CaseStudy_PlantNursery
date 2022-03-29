14fe0c233a7f7d3c017b1838ef3f50883c92011e
Mayank Kumar <71247904+mayank-kumar29@users.noreply.github.com>
2022-03-30 03:13:59 +0530

    Update ICartRepository.java

--- src/main/java/com/cs/repository/ICartRepository.java
+++ src/main/java/com/cs/repository/ICartRepository.java
@@ -3,7 +3,12 @@ package com.cs.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import com.cs.bean.Cart;
-
+/**
+ * This interface acts as repository which communicates with database and
+ * executes all the queries coming from CartService to database
+ * @author  Mayank Kumar(Employee ID: 46191925)
+ *
+ */
 @Repository
 public interface ICartRepository extends JpaRepository<Cart, Integer>{
 
