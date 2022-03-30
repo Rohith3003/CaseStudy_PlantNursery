db44542448d7e8d0a2e697875df58dea049b2d28
sapalasrusti <vsapalasrusti@gmail.com>
2022-03-30 09:30:05 +0530

    EndUser Commit

--- src/main/java/com/cs/bean/EndUser.java
+++ src/main/java/com/cs/bean/EndUser.java
@@ -1,7 +1,6 @@
 package com.cs.bean;
 
 import java.util.Date;
-import java.util.List;
 
 import javax.persistence.CascadeType;
 import javax.persistence.Column;
@@ -11,7 +10,6 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
-import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
 
 import org.springframework.data.annotation.CreatedDate;
@@ -68,9 +66,8 @@ public class EndUser {
 	@JoinColumn(name = "cart_Id")
 	private Cart cart;
 
-    
-    @OneToOne(cascade = CascadeType.ALL)
-    @JoinColumn(name = "order_id")
-    private OrderDetails order;
+	@OneToOne(cascade = CascadeType.ALL)
+	@JoinColumn(name = "order_id")
+	private OrderDetails order;
 
 }
