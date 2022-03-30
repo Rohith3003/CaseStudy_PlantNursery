b91ebe723765acb6b0151a8cb60d22d4b4298318
hpedamal <hpedamal@LIN24010063.corp.capgemini.com>
2022-03-30 12:17:50 +0530

    Added few testcases

--- src/test/java/com/cs/service/PlanterServiceTest.java
+++ src/test/java/com/cs/service/PlanterServiceTest.java
@@ -20,22 +20,18 @@ public class PlanterServiceTest {
 	IPlanterService planterService;
 	
 	Planter returned_planter;
+	
+	/**
+	 * JUnit test case to test the add planter functionality of
+	 * PlanterService class.
+	 */
 	@Test 
 	@Disabled
 	public void addPlanterTest()
 	{
 		//Creating a planter object and setting it's attributes
-		Planter planter=new Planter();
-		planter.setCategory(Type.CERMAIC_POT);
-		planter.setDescription("This is Ceramic type pot");
-		planter.setName("Cream Stripes M");
-		planter.setPhotoLoc("https://m.media-amazon.com/images/I/81wOdvLuauL._SL1500_.jpg");
-		planter.setPrice(650);
-//		planter.setCategory(Type.PLASTIC_POT);
-//		planter.setDescription("Kraft seeds have always believed in sustainable development of environment by believing in growing a seed a house.");
-//		planter.setName("Brown M plain");
-//		planter.setPhotoLoc("https://m.media-amazon.com/images/I/71v16h4eX7L._SX679_.jpg");
-//		planter.setPrice(800);
+		Planter planter=new Planter(5,"https://m.media-amazon.com/images/I/71t0G89Pi-L._SX522_.jpg","Cream Stripes M",Type.CERMAIC_POT,"This is Ceramic type pot",650);
+//		Planter planter=new Planter(6,"https://m.media-amazon.com/images/I/71v16h4eX7L._SX679_.jpg","Brown M plain",Type.PLASTIC_POT,"Kraft seeds have always delivered best",800);
 		returned_planter=planterService.addPlanter(planter,1);
 		assertEquals("This is Ceramic type pot", returned_planter.getDescription());
 		assertEquals(Type.CERMAIC_POT,returned_planter.getCategory());
@@ -44,19 +40,27 @@ public class PlanterServiceTest {
 		assertEquals(650, returned_planter.getPrice());
 	}
 	
+	/**
+	 * JUnit test case to test the get planter by id functionality of
+	 * PlanterService class.
+	 */
 	@Test
 	//@Disabled
 	public void getPlanterByIdTest()
 	{
-		Planter returned_planter=planterService.getPlanterById(9);
+		returned_planter=planterService.getPlanterById(10);
 		//Comparing all the attributes of returned product
-		assertEquals("Kraft seeds have always believed in sustainable development of environment by believing in growing a seed a house.", returned_planter.getDescription());
-		assertEquals(Type.PLASTIC_POT, returned_planter.getCategory());
-		assertEquals("Brown M plain", returned_planter.getName());
-		assertEquals("https://m.media-amazon.com/images/I/71v16h4eX7L._SX679_.jpg", returned_planter.getPhotoLoc());
-		assertEquals(800, returned_planter.getPrice());
+		assertEquals("This is Ceramic type pot", returned_planter.getDescription());
+		assertEquals(Type.CERMAIC_POT,returned_planter.getCategory());
+		assertEquals("Cream Stripes M", returned_planter.getName());
+		assertEquals("https://m.media-amazon.com/images/I/71t0G89Pi-L._SX522_.jpg", returned_planter.getPhotoLoc());
+		assertEquals(555, returned_planter.getPrice());
 	}
 	
+	/**
+	 * JUnit test case to test the get planter by name functionality of
+	 * PlanterService class.
+	 */
 	@Test
 	//@Disabled
 	public void getPlanterByNameTest()
@@ -66,52 +70,73 @@ public class PlanterServiceTest {
 		assertEquals("This is Ceramic type pot", returned_planter.getDescription());
 		assertEquals(Type.CERMAIC_POT,returned_planter.getCategory());
 		assertEquals("Cream Stripes M", returned_planter.getName());
-		assertEquals("https://m.media-amazon.com/images/I/81wOdvLuauL._SL1500_.jpg", returned_planter.getPhotoLoc());
-		assertEquals(650, returned_planter.getPrice());
+		assertEquals("https://m.media-amazon.com/images/I/71t0G89Pi-L._SX522_.jpg", returned_planter.getPhotoLoc());
+		assertEquals(555, returned_planter.getPrice());
 	}
 	
+	/**
+	 * JUnit test case to test the delete planter by id functionality of
+	 * PlanterService class.
+	 */
 	@Test
 	//@Disabled
 	public void deletePlanterByIdTest()
 	{
-		Planter returned_planter=planterService.deletePlanterById(9,1);
-		//Comparing all the attributes of returned product
-		assertEquals("Kraft seeds have always believ", returned_planter.getDescription());
-		assertEquals(Type.PLASTIC_POT,returned_planter.getCategory());
-		assertEquals("Brown M plain", returned_planter.getName());
-		assertEquals("https://m.media-amazon.com/images/I/71v16h4eX7L._SX679_.jpg", returned_planter.getPhotoLoc());
-		assertEquals(555, returned_planter.getPrice());
-	}
-	
-	@Test
-	//@Disabled
-	public void updatePlanterPriceTest()
-	{	//Updates the price and returns modified object
-		Planter returned_planter=planterService.updatePlanterPrice(10,555,1);
+		Planter returned_planter=planterService.deletePlanterById(10,1);
 		//Comparing all the attributes of returned product
 		assertEquals("This is Ceramic type pot", returned_planter.getDescription());
 		assertEquals(Type.CERMAIC_POT,returned_planter.getCategory());
 		assertEquals("Cream Stripes M", returned_planter.getName());
-		assertEquals("https://m.media-amazon.com/images/I/81wOdvLuauL._SL1500_.jpg", returned_planter.getPhotoLoc());
+		assertEquals("https://m.media-amazon.com/images/I/71t0G89Pi-L._SX522_.jpg", returned_planter.getPhotoLoc());
 		assertEquals(555, returned_planter.getPrice());
 	}
 	
+	/**
+	 * JUnit test case to test the update planter price functionality of
+	 * PlanterService class.
+	 */
+	@Test
+	//@Disabled
+	public void updatePlanterPriceTest()
+	{	//Updates the price and returns modified object
+		Planter returned_planter=planterService.updatePlanterPrice(13,333,1);
+		//Comparing all the attributes of returned product
+		assertEquals("Iron Floor Set of 2.", returned_planter.getDescription());
+		assertEquals(Type.PLASTIC_POT,returned_planter.getCategory());
+		assertEquals("Iron pots dual long set", returned_planter.getName());
+		assertEquals("https://5.imimg.com/data5/US/JS/ZH/SELLER-28215188/set-of-2-modern-planter-with-stand-500x500.jpg", returned_planter.getPhotoLoc());
+		assertEquals(333, returned_planter.getPrice());
+	}
+	
+	/**
+	 * JUnit test case to test the get all planters functionality of
+	 * PlanterService class.
+	 */
 	@Test
 	public void getAllPlantersTest()
 	{
 		List<PlanterDto> planters=planterService.getAllPlanters();
-		assertEquals(1, planters.size());
+		assertEquals(4, planters.size());
 	}
 	
+	/**
+	 * JUnit test case to test the update planter photo by Id functionality of
+	 * PlanterService class.
+	 */
 	@Test
 	public void updatePlanterPhotoTest()
 	{
-		Planter returned_planter=planterService.updatePlanterPhoto(10, "https://m.media-amazon.com/images/I/71t0G89Pi-L._SX522_.jpg",1);
-		assertEquals(10, returned_planter.getPlanterId());
+		Planter returned_planter=planterService.updatePlanterPhoto(13, "https://m.media-amazon.com/images/I/71t0G89Pi-L._SX522_.jpg",1);
+		assertEquals(13, returned_planter.getPlanterId());
 		assertEquals("https://m.media-amazon.com/images/I/71t0G89Pi-L._SX522_.jpg", returned_planter.getPhotoLoc());
 	}
 	
+	/**
+	 * JUnit test case to test the delete planter by name functionality of
+	 * PlanterService class.
+	 */
 	@Test
+	@Disabled
 	public void deletePlanterByNameTest()
 	{
 		assertEquals(1, planterService.getAllPlanters().size());
@@ -120,13 +145,16 @@ public class PlanterServiceTest {
 		assertEquals("Cream Stripes M", returned_planter.getName());
 	}
 	
+	/**
+	 * JUnit test case to test the update planter price by name functionality of
+	 * PlanterService class.
+	 */
 	@Test
-	@Disabled
 	public void updatePlanterPriceByNameTest()
 	{
-		Planter returned_planter=planterService.getPlanterByName("Testy");
-		returned_planter=planterService.updatePlanterPrice("Testy", 333,1);
-		assertEquals("Testy", returned_planter.getName());
-		assertEquals(333, returned_planter.getPrice());
+		Planter returned_planter=planterService.getPlanterByName("Faceted Modern");
+		returned_planter=planterService.updatePlanterPrice("Faceted Modern", 999,1);
+		assertEquals("Faceted Modern", returned_planter.getName());
+		assertEquals(999, returned_planter.getPrice());
 	}
 }
